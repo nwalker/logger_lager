850c7e436495ff64f51bc98353671c717ba71797
Yuri Artemev <i@artemeff.com>
2016-07-18 16:13:39 +0300

    add metadata formatting

--- lib/logger_lager_backend.ex
+++ lib/logger_lager_backend.ex
@@ -16,11 +16,34 @@ defmodule LoggerLagerBackend do
     {:ok, :ok, state}
   end
 
+  def handle_event({level, _gl, {Logger, message, timestamp, metadata}}, state) do
+    :lager.dispatch_log(@sink, to_lager_level(level), format_metadata(metadata),
+      '~ts', [message], @truncation_size, :safe)
+    {:ok, state}
+  end
+
   defp to_lager_level(:warn), do: :warning
   defp to_lager_level(level), do: level
 
-  def handle_event({level, _groupleader, {Logger, message, _timestamp, metadata}}, state) do
-    :lager.dispatch_log(@sink, to_lager_level(level), metadata, '~ts', [message], @truncation_size, :safe)
-    {:ok, state}
+  defp format_metadata(md) do
+    Enum.map(md, fn({key, val}) ->
+      {key, [to_string(key), ?=, format_metadata_value(val), ?\s]}
+    end)
   end
+
+  defp format_metadata_value(pid) when is_pid(pid) do
+    :erlang.pid_to_list(pid)
+  end
+  defp format_metadata_value(ref) when is_reference(ref) do
+    '#Ref' ++ rest = :erlang.ref_to_list(ref)
+    rest
+  end
+  defp format_metadata_value(atom) when is_atom(atom) do
+    case Atom.to_string(atom) do
+      "Elixir." <> rest -> rest
+      "nil" -> ""
+      binary -> binary
+    end
+  end
+  defp format_metadata_value(other), do: to_string(other)
 end
